5f1ea946cf96ec0a9dffc67d20f7051124a1a564
alantva <alan_tva_90@yahoo.com.br>
2018-10-03 11:51:47 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to Wesley.
+            Edit <code>src/App.js</code> and save to Desirée.
           </p>
           <a
             className="App-link"
